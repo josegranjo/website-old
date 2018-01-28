f61c63e5d104e99f0cd6d12c82a7c5cf948d4e1d
Jose Granjo <josegranjo@spotify.com>
2018-01-28 15:06:08 +0100

    Hello hyperapp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { h, app } from "hyperapp"
+
+const state = {
+  count: 0
+}
+
+const actions = {
+  down: () => state => ({ count: state.count - 1 }),
+  up: () => state => ({ count: state.count + 1 })
+}
+
+const view = (state, actions) => (
+  <main>
+    <h1>{state.count}</h1>
+    <button onclick={actions.down}>-</button>
+    <button onclick={actions.up}>+</button>
+  </main>
+)
+
+const main = app(state, actions, view, document.body)
