db2ae4d220e0f2fef4a5eb81a08820e85c508297
Jose Granjo <josegranjo@spotify.com>
2018-02-04 10:49:59 +0100

    Remove old site

--- src/Content.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Content = (props) => (
-  <div className='content'>
-    { props.children }
-  </div>
-)
-
-export default Content
--- src/Publications.js
+++ src/Publications.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Publications = () => (
   <div>
     <h4>Publications</h4>
